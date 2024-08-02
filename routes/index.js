518c493394852336e64dda85ef0c453728ea0192
Jay Lin <linshaochieh2019@gmail.com>
2024-08-02 09:38:02 +0800

    Project initialization

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const packageJson = require('../package.json'); // Import package.json
+
+// Entry point
+router.get("/", (req, res) => {
+  const data = {
+    message: "Hello World!",
+    timestamp: new Date().toISOString(), // Add a timestamp for demonstration
+  };
+  res.json(data); // Send JSON data
+});
+
+// Return node server package version
+router.get("/version", (req, res) => {
+  const data = {
+    version: packageJson.version,
+  };
+  res.json(data);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+// Router
+const indexRouter = require("./routes/index");
+
+// Middleware
+app.use(express.json());
+
+// Configure CORS to allow requests from your Angular app
+app.use(
+  cors({
+    origin: ["https://paigonpig.web.app", "http://localhost:4200"],
+  })
+);
+
+// Routes
+app.use("/", indexRouter);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
